712296dbfb61c698fa90a067596b406f47ea2f70
MBudzynski <michalbu92@gmail.com>
2024-10-10 22:35:26 +0200

    Add new module queue-system-facility

---
+++ queuesystem-facility/src/main/java/com/example/queuesystemfacility/QueuesystemFacilityApplication.java
@@ -0,0 +1,13 @@
+package com.example.queuesystemfacility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuesystemFacilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QueuesystemFacilityApplication.class, args);
+    }
+
+}
